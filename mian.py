8a8adcec09b75854601a76c70a0d378db2a8b069
KAkos27 <akoskosztolanyi@gmail.com>
2024-01-17 14:53:10 +0100

    eldontes

--- mian.py
+++ mian.py
@@ -3,7 +3,9 @@ while n < 0:
     n = int(input("N = "))
 
 ossz = 0
-for i in range(0, n + 1, 1):
+for i in range(1, n + 1, 1):
     ossz += i
 
 print(f"Az első {n}db természetes szám összege: {ossz}")
+
+
